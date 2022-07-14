6a49975eb0c94c3974f5d82564351be1f059ccb9
Spec-75 <Spec_75@mail.ru>
2022-07-14 18:40:30 +0300

    add task_36

---
+++ task_36/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Enter the number of elements in the array");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int []array =new int[number];
+int size = array.Length;
+int sumNotEvenNumber = 0;
+for (int i = 0; i< size; i++)
+{
+    array[i] = new Random().Next();
+
+    if(i % 2 != 0)
+    {
+        sumNotEvenNumber += array[i];
+    }
+}
+
+Console.WriteLine($"sum of odd array elements: {sumNotEvenNumber}");
+Console.WriteLine($"Source array: [{String.Join(", ", array)}]");
