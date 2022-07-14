16ca619737481a6d1b52735009b5f1d9fc41b324
Spec-75 <Spec_75@mail.ru>
2022-07-14 19:27:41 +0300

    add task_36

--- task_36/Program.cs
+++ task_36/Program.cs
@@ -8,16 +8,16 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int []array =new int[number];
 int size = array.Length;
-int sumNotEvenNumber = 0;
+int sumOddElement = 0;
 for (int i = 0; i< size; i++)
 {
     array[i] = new Random().Next();
 
     if(i % 2 != 0)
     {
-        sumNotEvenNumber += array[i];
+        sumOddElement += array[i];
     }
 }
 
-Console.WriteLine($"sum of odd array elements: {sumNotEvenNumber}");
+Console.WriteLine($"sum of odd array elements: {sumOddElement}");
 Console.WriteLine($"Source array: [{String.Join(", ", array)}]");
